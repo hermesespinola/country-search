db11853a37fa23ad0054f48a983d99814fbad90e
Hermes <hermes.espinola@gmail.com>
2020-03-21 21:56:58 -0700

    Autocomplete placeholder

--- client/src/components/container/CountryAutocomplete.js
+++ client/src/components/container/CountryAutocomplete.js
@@ -5,11 +5,15 @@ import { autcompleteClosestCountries } from '../../api';
 import { useDebounce } from '../../hooks';
 
 const CountryAutocomplete = () => {
+    // debouncedQuery to only trigger the api call when
+    // the user stoped for some time.
     const [debouncedQuery, query, setQuery] = useDebounce('', 300);
     const [countries, setCountries] = useState([]);
-    const [showResults, setShowResults] = useState(true);
+    const [showResults, setShowResults] = useState(false);
 
     useEffect(() => {
+        // Don't fetch data if the query is empty or
+        // if we don't want to show the results.
         if (!debouncedQuery || !showResults) {
             return;
         }
@@ -24,6 +28,8 @@ const CountryAutocomplete = () => {
             <AutocompleteItem
                 key={`${postal}-${name}`}
                 onClick={() => {
+                    // When clicked an item we want to update the query
+                    // and stop fetching/showing country results.
                     setShowResults(false);
                     setCountries([]);
                     setQuery(name);
@@ -38,8 +44,10 @@ const CountryAutocomplete = () => {
     return (
         <Autocomplete
             label="Find the closest country"
+            placeholder={"Country"}
             value={query}
             onChange={(value) => {
+                // If the user starts typing again, show the results.
                 if (!showResults) {
                     setShowResults(true);
                 }
--- client/src/components/presentational/Autocomplete/Autocomplete.js
+++ client/src/components/presentational/Autocomplete/Autocomplete.js
@@ -4,12 +4,13 @@ import ResultList from './ResultList';
 import Label from '../Label';
 import './Autocomplete.css';
 
-const Autocomplete = ({ children, label, value, onChange }) => (
+const Autocomplete = ({ children, label, placeholder, value, onChange }) => (
     <div className="autocomplete-container">
         {label && <Label color="#a3d3ff">Find the closest country</Label>}
         <input
             className="autocomplete-input"
             value={value}
+            placeholder={placeholder}
             onChange={(event) => {
                 onChange(event.target.value);
             }}
