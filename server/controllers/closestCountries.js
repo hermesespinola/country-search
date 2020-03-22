83eb5c1a6b9b654b07bde00565b15f82924e3a16
Hermes <hermes.espinola@gmail.com>
2020-03-21 20:33:07 -0700

    Sort countries by current location

--- server/controllers/closestCountries.js
+++ server/controllers/closestCountries.js
@@ -1,13 +1,16 @@
 const countriesService = require('../services/countries');
 const locationService = require('../services/location');
+const { getRequestAddress } = require('../util');
 
 const DEFAULT_LIMIT = 5;
 
 async function closestCountries(req, res) {
     const { name } = req.query;
     const limit = parseInt(req.query.limit) || DEFAULT_LIMIT;
+    const clientAddress = getRequestAddress(req);
+
     try {
-        const location = await locationService.getLocation(req.connection.remoteAddress);
+        const location = await locationService.getLocation(clientAddress);
         const countries = countriesService.closestCountries(name, limit, location);
         res.send(countries);
     } catch (error) {
--- server/services/countries.js
+++ server/services/countries.js
@@ -4,11 +4,10 @@ const countriesFilePath = process.env.COUNTRIES_FILE;
 const countriesFile = fs.readFileSync(countriesFilePath, 'utf8');
 const contents = JSON.parse(countriesFile);
 
+/** @type {{ name: string, postal: string, lat: number, lng: number }[]} */
 const countriesMetadata = contents.countries.map(
     ({ name, postal, lat, lng }) => ({ name, postal, lat, lng }),
 );
-
-// TODO: index based in location
 
 const countriesService = {
     /**
@@ -18,13 +17,41 @@ const countriesService = {
      */
     closestCountries(queryName, limit, location) {
         const queryLower = queryName.toLowerCase();
-        console.log('req location:', location);
-        const nameMatch = countriesMetadata.filter(
-            ({ name }) => name.toLowerCase().startsWith(queryLower),
-        );
-        console.log({ nameMatch });
-        return nameMatch.slice(0, limit);
+        const nameStartsWithQuery = ({ name }) => name.toLowerCase().startsWith(queryLower);
+        const closestToLocation = (c1, c2) => geoDistance(c1, location) - geoDistance(c2, location);
+
+        const countriesMatch = countriesMetadata
+            .filter(nameStartsWithQuery)
+            .sort(closestToLocation)
+            .slice(0, limit);
+
+        return countriesMatch;
     },
 };
+
+const R = 6371; // Radius of the earth in km
+
+function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+}
+
+/**
+ * Calculate distance between two points
+ * @param {{lat: number, lng: number}} p1
+ * @param {{lat: number, lng: number}} p2
+ */
+function geoDistance(p1, p2) {
+    const deltaLat = deg2rad(p2.lat - p1.lat);
+    const deltaLng = deg2rad(p2.lng - p1.lng); 
+
+    const a = 
+      Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
+      Math.cos(deg2rad(p1.lat)) * Math.cos(deg2rad(p2.lat)) *
+      Math.sin(deltaLng / 2) * Math.sin(deltaLng / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); 
+    const distance = R * c; // Distance in km
+    return distance;
+}
 
 module.exports = countriesService;
